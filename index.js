fbc096aa3552335cd332bef89e20a124ea6031a5
Naveen <naveen.thakur@prepladder.com>
2023-12-22 12:50:06 +0530

    2nd

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // import { abc },abc_def from "./modules/test";
 import test, { test_2 } from "./modules/test.js";
+import kjhjk, { test_2 as aaa } from "./modules/moduleone.js";
 
 test();
 test_2();
+
+aaa();
+kjhjk();
---
+++ modules/moduleone.js
@@ -0,0 +1,8 @@
+// test.js
+export default function test() {
+  console.log("This is the test module.");
+}
+
+export function test_2() {
+  console.log("This is the test 2 module.");
+}
